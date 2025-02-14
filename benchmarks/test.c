008a47766b3f29947dffd2470f0206f8ad4c9120
Jay Kania <120383316+jpkOSTEP@users.noreply.github.com>
2025-02-13 23:18:50 -0500

    Pushing thread library & scheduler code as well as writeup

---
+++ benchmarks/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "../worker.h"
+
+/* A scratch program template on which to call and
+ * test worker library functions as you implement
+ * them.
+ */
+int main(int argc, char **argv) {
+
+	/* Need to Implement HERE */
+
+	return 0;
+}
